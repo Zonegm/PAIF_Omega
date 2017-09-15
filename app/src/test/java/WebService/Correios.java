d5ce9c3d21617a01bd6f58d708655c1dbb943c91
logonrm <logonrm@fiap.com.br>
2017-09-15 08:51:12 -0300

    Initial commit

---
+++ app/src/test/java/WebService/Correios.java
@@ -0,0 +1,12 @@
+package WebService;
+
+public class Correios {
+
+    "cep": "76873274",
+    "tipo": "Rua",
+    "logradouro":"Juriti",
+    "bairro":"Setor 02",
+    "cidade":"Ariquemes",
+    "estado":"RO",
+
+}
